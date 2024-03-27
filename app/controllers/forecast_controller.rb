f74464580d5f48d29def1962b57bb03579008eb5
Damon Davison <damon@allolex.net>
2024-03-27 17:54:22 -0400

    Add caching support

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -4,10 +4,21 @@ class ForecastController < ApplicationController
   end
 
   def display
-    @forecast = ForecastService.get(forecast_params).to_hash
+    @fetched_from_cache = true
+
+    @forecast = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
+      @fetched_from_cache = false
+      ForecastService.get(forecast_params).to_hash
+    end
   end
 
 private
+
+  # Use only lower case word characters, digits, or a comma to create cache key.
+  # This allows caching on location names and postcodes/zip codes that have spaces etc in them.
+  def cache_key
+    "#{forecast_params.downcase.scan(/[\w\d,]/)}/forecast"
+  end
 
   def forecast_params
     params.require(:query)
